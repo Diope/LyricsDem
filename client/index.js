bec607a5f9e590c5d8ab65d613e0e7cdee57008b
Dion Pettaway <dionp@fullsail.edu>
2018-02-12 18:14:44 -0500

    React Router

--- client/index.js
+++ client/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+
+import App from './components/App';
+import SongList from './components/SongList';
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        LyricsDem
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
