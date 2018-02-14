01c6c5458cfcafe167e8613da18749aaba0ab67f
Dion Pettaway <dionp@fullsail.edu>
2018-02-14 16:42:36 -0500

    apollo bug

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+
+import likeLyricMutation from '../queries/likeLyricMutation'
 
 class LyricList extends Component {
 
+  likeLyric(id, likes) {
+
+    this.props.mutate({
+      variables: {id: id},
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id: id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    });
+  }
+
   renderLyrics() {
-    return this.props.songLyrics.map(({id, content}) => {
+    return this.props.songLyrics.map(({id, content, likes}) => {
       return (
         <li className="collection-item" key={id}>
           {content}
+          <div className="voting-box">
+            <i className="material-icons"
+                onClick={() => this.likeLyric(id, likes)}
+              >thumb_up</i>
+              {likes}
+          </div>
         </li>
       )
     })
@@ -21,4 +45,4 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(likeLyricMutation)(LyricList);
